8e79d6d7a79d303185ac2b1278d730ba7ef09407
Marcus Åkerman <marcus.akerman@learnet.se>
2018-10-22 09:40:42 +0200

    Tränat på HTML, css, target, listener och textContent

---
+++ js_grunder/korstracka.js
@@ -0,0 +1,33 @@
+/* 
+ * Räknar ut en körsträckan för en bil.
+ * Mata in hastighet och tid.
+ * Få avtsånd som svar.
+ */
+
+/* Kör koden när webbsidan laddats klart */
+window.onload = start;
+
+function start() {
+
+    /* Välj ut alla elementen att jobba med: target */
+    const elementHastighet = document.querySelector('.hastighet')
+    const elementTid = document.querySelector('.tid')
+    const elementRaknaUT = document.querySelector('.raknaUt')
+    const elementSvar = document.querySelector('.svar')
+    /* Lyssna på knappen */
+    elementRaknaUT.addEventListener('click', raknaUt);
+
+    /* Räkna ut avståndet */
+    function raknaUt() {
+        
+        /* Läs av hastigheten och tiden */
+        var hastighet = elementHastighet.textContent;
+        var tid = elementTid.textContent;
+
+        /* Räkna ut avståndet = hastigheten * tid */
+        var avstand = hastighet * tid / 60;
+
+        /* Skriv ut resultatet tex 'Körsträckan blir 57 km.' */
+        elementSvar.textContent = 'Körsträcan blir ' + avstand + ' km.';
+    }
+}
