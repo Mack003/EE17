0bd3e4e0c91fc77c191e8b9fa0d9dd1285919ef5
Marcus Åkerman <marcus.akerman@learnet.se>
2018-10-25 13:30:56 +0200

    Tränat på klasser och queryselector

---
+++ js_grunder/bildgalleri.js
@@ -0,0 +1,20 @@
+/* När man klickar på en knapp byts bilden ut till aktuell bild */
+
+/* När webbsidan laddats klart kör start() */
+window.onload = start;
+
+/* Start Delen */
+function start() {
+
+    /* Vilka element arbetar vi med? */
+    const elementImg = document.querySelector('.bild img');
+    const elementP = document.querySelector('.bild p');
+    const elementKnapp1 = document.querySelector('.knapp1')
+    const elementKnapp1 = document.querySelector('.knapp2')
+    const elementKnapp1 = document.querySelector('.knapp3')
+    const elementKnapp1 = document.querySelector('.knapp4')
+
+    /* Vilka händelser ska vi lyssna på? */
+
+    /* Vilka funktioner ska vi ha? */
+}
